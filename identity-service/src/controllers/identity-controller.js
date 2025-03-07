fff27bf5fc4b6f58e6b68afb45017cf6cf394507
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-03-07 14:54:23 +0530

    Add login user controller

--- identity-service/src/controllers/identity-controller.js
+++ identity-service/src/controllers/identity-controller.js
@@ -1,7 +1,7 @@
 const User = require("../models/User.js");
 const generateTokens = require("../utils/generateToken.js");
 const logger = require("../utils/logger.js");
-const { validateRegistration } = require("../utils/validation.js");
+const { validateRegistration, validateLogin } = require("../utils/validation.js");
 
 // user registration
 const registerUser = async (req, res) => {
@@ -52,10 +52,57 @@ const registerUser = async (req, res) => {
 };
 
 // user login
+const loginUser = async(req, res)=>{
+  logger.info("Login endpoint hit");
+  try {
+    const {error} = validateLogin(req.body);
+    if (error) {
+      logger.warn("Validation error", error.details[0].message);
+      return res.status(400).json({
+        success: false,
+        message: error.details[0].message,
+      });
+    }
+    const {email, password} = req.body;
+    const user = await User.findOne({email})
+    if(!user){
+      logger.warn("Invalid user")
+      res.status(400).json({
+        success: false,
+        message: "Invalid credentials"
+      })
+    }
+
+    // check user valid password
+    const isValidPassword = await user.comparePassword(password)
+    if(isValidPassword){
+      logger.warn("Invalid password")
+      res.status(400).json({
+        success: false,
+        message: "Invalid password"
+      })
+    }
+
+    const {accessToken, refreshtoken} = await generateTokens(user)
+
+    res.json({
+      accessToken,
+      refreshtoken,
+      userID: user._id
+    })
+
+  } catch (error) {
+    logger.error('Login error occured', error);
+    res.status(500).json({
+        success: false,
+        message: 'Some Internal error'
+    })
+  }
+}
 
 // refresh token
 
 // logout
 
 
-module.exports = {registerUser}
+module.exports = {registerUser, loginUser}
